928274f8d525a6dab311bce265206782e00ff5ec
Ojas <ojasgramo@gmail.com>
2020-12-08 01:01:52 +0530

    logout issue resolved

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,20 +8,32 @@ import { Router } from '@angular/router';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  	page : String;
 
-  constructor(private userSer: UserServiceService, private router : Router) { }
+  constructor(private userSer: UserServiceService, private router : Router) {
+	  this.page = window.location.href.split("/")[window.location.href.split("/").length-1]
+  }
 
   ngOnInit(): void {
   }
   foo = 1;
   log = localStorage.getItem('userToken') !== null;
-  
   //log2 = true;
 
   Logout() {
     //alert(this.userSer.loggedIn);
     this.userSer.LogoutUser();
     //alert(this.userSer.loggedIn);
+  }
+
+  Disp(log : Boolean, page : String){
+  	if(log){
+  		if(page!="login"){
+  			return true;
+  		}
+  		else
+  			return false;
+  	}
   }
 
 
